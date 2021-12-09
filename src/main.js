de08ce301c3cece4fbe9b54813cea46a4080acc3
xunxas0410 <60127190+xunxas0410@users.noreply.github.com>
2021-12-09 21:30:58 +0800

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
+// 引入vue
 import Vue from 'vue'
+// 引入 App
 import App from './App.vue'
-
+// 關閉vue的生產提示
 Vue.config.productionTip = false
 
+// 創建vm
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    el:'#app',
+  render: h => h(App)
+})
