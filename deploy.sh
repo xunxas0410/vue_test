a16b5df7543f27f9b3cea7f221e715af9504f895
xunxas0410 <60127190+xunxas0410@users.noreply.github.com>
2021-12-14 22:19:41 +0800

    cli push test

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,20 @@
-git push -f https://github.com/xunxas0410/Not_just_todoList.git//deploy.sh
 #!/usr/bin/env sh
 # 當發生錯誤時終止腳本運行
 set -e
-# 打包
+# 建立輸出檔案
 npm run build
+
 # 移動至到打包後的dist目錄 
 cd dist
-git init //因為dist資料夾預設是被ignore的，因此在進入dist資料夾後初始化git
+
+# 因為dist資料夾預設是被ignore的，因此在進入dist資料夾後初始化git
+git init 
 git add -A
 git commit -m 'deploy'
-# 部署到 https://github.com/chou0728/eric-project.git 分支為 gh-pages
-git push -f https://github.com/chou0728/eric-project.git master:gh-pages
-//將dist資料夾中的內容推送至遠端eric-project的gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
+
+# 將 dist資料夾中的內容推送至遠端 hexWeek6Demo的 gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
+git push -f https://github.com/xunxas0410/VueTodoList.git master:gh-pages
+
 cd -
+
+# 執行指令，在終端機輸入 deploy.sh
